006c590c80da9cd965655755ff99a8c0d1f5f61c
Pierre-Rudolf Gerlach <pgerlach@gmail.com>
2013-10-19 16:40:18 +0200

    modifs

--- server.js
+++ server.js
@@ -11,17 +11,37 @@ server.listen(port);
 
 console.log('http server listening on %d', port);
 
+var beercount = 0;
+
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
+
+var clients = [];
+
 wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
+
+  console.log(ws);
+    clients.push(ws);
+
+    // var id = setInterval(function() {
+    //     ws.send(JSON.stringify(new Date()), function() {  });
+    // }, 1000);
 
     console.log('websocket connection open');
 
     ws.on('close', function() {
         console.log('websocket connection close');
         clearInterval(id);
+        // TODO remove from clients
     });
+});
+
+app.get("/newbeer", function(req, resp) {
+  beercount++;
+  for (i in clients) {
+    ws = clients[i];
+    console.log(ws);
+    ws.send(JSON.stringify(beercount), function() {  });
+  }
+  resp.send("" + beercount);
 });
